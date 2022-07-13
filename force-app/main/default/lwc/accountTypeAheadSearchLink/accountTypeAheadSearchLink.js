b82fa9ea2d5b4734e216be45753c9f4396f1c2ce
jkurant <jkurant@TheHunleyGroup.com>
2022-07-13 16:49:28 -0400

    Made the link a child component. Could not indent.

--- force-app/main/default/lwc/accountTypeAheadSearchLink/accountTypeAheadSearchLink.js
+++ force-app/main/default/lwc/accountTypeAheadSearchLink/accountTypeAheadSearchLink.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class AccountTypeAheadSearchLink extends LightningElement {}
+export default class AccountTypeAheadSearchLink extends LightningElement {
+
+  @api link = '';
+  @api name = '';
+  @api indentlevel = 0;
+
+  get indentSpaces() {
+    let spaces = '';
+    for (let i = 0; i < this.indentlevel; i++) {
+      spaces += '&nbsp;'
+    }
+    //console.log('indentlevel = ' + this.indentlevel + '; spaces = [' + spaces + ']');
+    return spaces;
+  }
+
+}
